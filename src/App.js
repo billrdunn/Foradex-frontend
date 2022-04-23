b6b26fb4e45b40145d421ae8b8bf8a32d10c1581
Bill Dunn <billrdunn@gmail.com>
2022-04-23 10:04:05 +0100

    Add functionality for user to log in.

--- src/App.js
+++ src/App.js
@@ -2,19 +2,38 @@ import { useEffect, useState } from "react";
 import Item from "./components/Item";
 import SearchBar from "./components/SearchBar";
 import itemService from "./services/items";
+import loginService from "./services/login";
 
 const App = () => {
   const [items, setItems] = useState([]);
   const [showAll, setShowAll] = useState(true);
   const [searchStr, setSearchStr] = useState(" ");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    itemService
-      .getAll()
-      .then(res => {
-        setItems(res)
-      })
-  }, [])
+    itemService.getAll().then((res) => {
+      setItems(res);
+    });
+  }, []);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    console.log("logging in with", username, password);
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+      itemService.setToken(user.token);
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      alert("Invalid credentials", exception);
+    }
+  };
 
   const itemsToShow = showAll
     ? items
@@ -30,9 +49,34 @@ const App = () => {
     setSearchStr(event.target.value.toLowerCase());
   };
 
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  );
+
   return (
     <div>
       <h1>Foradex</h1>
+      {user === null ? loginForm() : <h2>{user.name} logged in</h2>}
       {itemsToShow.map((item) => (
         <Item key={item.id} item={item} />
       ))}
