90ed9d9ae720aef23b894a55e93421d3b3823c51
Bill Dunn <billrdunn@gmail.com>
2022-05-03 10:45:58 +0100

    Separate the navigation bar into its own component and use react-bootstrap Navbar to stylise it.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Route, Routes, Link, useMatch } from "react-router-dom";
+import { Route, Routes, useMatch } from "react-router-dom";
 import LogoutButton from "./components/LogoutButton";
 import { initLoggedInUser } from "./reducers/loginReducer";
 import { initItems } from "./reducers/itemsReducer";
 import { initUsers } from "./reducers/usersReducer";
 import ItemList from "./components/ItemList";
 import Home from "./components/Home";
+import Navigation from "./components/Navigation";
 import ItemDetailed from "./components/ItemDetailed";
 
 function App() {
   const dispatch = useDispatch();
-  const loggedInUser = useSelector((state) => state.loggedInUser);
   const items = useSelector((state) => state.items);
 
   // If the url matches "/items/:id", we can use the match variable to find the item
@@ -24,27 +24,9 @@ function App() {
     dispatch(initUsers());
   }, [dispatch]);
 
-  const padding = {
-    padding: 5,
-  };
-
   return (
     <div className="container">
-      <div>
-        <Link style={padding} to="/">
-          home
-        </Link>
-        {loggedInUser && (
-          <Link style={padding} to="/items">
-            items
-          </Link>
-        )}
-        {loggedInUser && (
-          <Link style={padding} to="/user">
-            user
-          </Link>
-        )}
-      </div>
+      <Navigation />
 
       <Routes>
         <Route path="/" element={<Home />} />
